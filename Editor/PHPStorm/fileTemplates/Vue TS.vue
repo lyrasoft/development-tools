9bfc82ea093a85afbb062420f98bbf30aaf9abe2
Simon Asika <asika32764@gmail.com>
2022-04-14 12:24:55 +0800

    Update by lyra-cli on: 2022-04-14 06:24:55

---
+++ Editor/PHPStorm/fileTemplates/Vue TS.vue
@@ -0,0 +1,15 @@
+<template>
+#[[$END$]]#
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+name: "${NAME}"
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
